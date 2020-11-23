5acdc579d6e910c3ddbf84674c329bae5cef64c2
Caleb Espinoza G <caleb.espinoza@outlook.com>
2020-11-23 11:47:27 -0400

    Commit the base code

---
+++ src/test/java/com/caleb/helloworlddemo/HelloWorldDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caleb.helloworlddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
