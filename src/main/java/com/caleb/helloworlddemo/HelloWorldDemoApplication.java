02f5c21788b898a12b4d6816dfa50fee193fea2d
Caleb Espinoza G <caleb.espinoza@outlook.com>
2020-11-23 15:50:21 -0400

    Add two examples about Hello World in Spring Boot - GetMapping and RequestMapping

--- src/main/java/com/caleb/helloworlddemo/HelloWorldDemoApplication.java
+++ src/main/java/com/caleb/helloworlddemo/HelloWorldDemoApplication.java
@@ -2,12 +2,25 @@ package com.caleb.helloworlddemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
+@RestController
+//@RequestMapping(value="hello")
 public class HelloWorldDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldDemoApplication.class, args);
     }
 
+    @GetMapping
+    public String sayHello (@RequestParam(value = "myName", defaultValue = "World") String name) {
+        return String.format("Hello %s!", name);
+    }
+
+    //@RequestMapping(value = "/{firstName}/{lastName}", method = RequestMethod.GET)
+    //public String hello ( @PathVariable("firstName") String firstName,
+    //                      @PathVariable("lastName") String lastName) {
+    //    return String.format("{\"message\" : \"Hello %s %s\"}", firstName, lastName);
+    //}
 }
