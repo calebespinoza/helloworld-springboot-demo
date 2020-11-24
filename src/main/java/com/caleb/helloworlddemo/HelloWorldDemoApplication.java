2774a4902d830547beddc8f4e160d10f6811141e
Caleb Espinoza G <caleb.espinoza@outlook.com>
2020-11-24 15:42:31 -0400

    Expose a hello message in JSON format

--- src/main/java/com/caleb/helloworlddemo/HelloWorldDemoApplication.java
+++ src/main/java/com/caleb/helloworlddemo/HelloWorldDemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 @RestController
-//@RequestMapping(value="hello")
+@RequestMapping(value="hello")
 public class HelloWorldDemoApplication {
 
     public static void main(String[] args) {
@@ -14,15 +14,15 @@ public class HelloWorldDemoApplication {
     }
 
     // First Demo
-    @GetMapping
-    public String sayHello (@RequestParam(value = "myName", defaultValue = "World") String name) {
-        return String.format("Hello %s!", name);
-    }
+    //@GetMapping
+    //public String sayHello (@RequestParam(value = "myName", defaultValue = "World") String name) {
+    //    return String.format("Hello %s!", name);
+    //}
 
     // Second Demo
-    //@RequestMapping(value = "/{firstName}/{lastName}", method = RequestMethod.GET)
-    //public String hello ( @PathVariable("firstName") String firstName,
-    //                      @PathVariable("lastName") String lastName) {
-    //    return String.format("{\"message\" : \"Hello %s %s\"}", firstName, lastName);
-    //}
+    @RequestMapping(value = "/{firstName}/{lastName}", method = RequestMethod.GET)
+    public String hello ( @PathVariable("firstName") String firstName,
+                          @PathVariable("lastName") String lastName) {
+        return String.format("{\"message\" : \"Hello %s %s\"}", firstName, lastName);
+    }
 }
